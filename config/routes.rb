d9827a0a09b016a3710a00e41debfce17d0b36d4
LUCIANO <nanotrigueiro@hotmail.com>
2017-09-08 00:28:40 +0000

    Final Static pages

--- config/routes.rb
+++ config/routes.rb
@@ -4,6 +4,7 @@ Rails.application.routes.draw do
   get 'static_pages/help'
 
   resources :users
-  root 'application#hello'
+  root 'static_pages#home'
+  #root 'application#hello'
   #root 'users#index'
 end
