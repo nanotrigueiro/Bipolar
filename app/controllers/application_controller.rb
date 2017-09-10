aa6ca1f20f304dd22e0b2164ac1620d9df847381
LUCIANO <nanotrigueiro@hotmail.com>
2017-09-10 03:03:20 +0000

    Implement basic login

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,7 +1,4 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
-  
-  def hello
-    render html: "Welcome!"
-  end
+  include SessionsHelper
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,8 @@
 Rails.application.routes.draw do
   
 
+  get 'sessions/new'
+
   resources :users
   root 'static_pages#home'
   get  '/help',    to: 'static_pages#help'
@@ -8,8 +10,7 @@ Rails.application.routes.draw do
   get 'static_pages/home'
   get  '/signup',  to: 'users#new'
   post '/signup',  to: 'users#create'
-  #get 'static_pages/help'
-  #get 'static_pages/about'
-  #root 'application#hello'
-  #root 'users#index'
+  get    '/login',   to: 'sessions#new'
+  post   '/login',   to: 'sessions#create'
+  delete '/logout',  to: 'sessions#destroy'
 end
