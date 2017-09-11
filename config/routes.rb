1d8a074da15a3e2b05882a82258f608aa119801b
LUCIANO <nanotrigueiro@hotmail.com>
2017-09-11 02:24:41 +0000

    final

--- config/routes.rb
+++ config/routes.rb
@@ -17,7 +17,7 @@ Rails.application.routes.draw do
   get  '/signup',  to: 'users#new'
   post '/signup',  to: 'users#create'
   get    '/login',   to: 'sessions#new'
-  post   '/login',   to: 'sessions#create'
+  post   '/login',   to: 'monitorings#index'
   delete '/logout',  to: 'sessions#destroy'
   get   '/monitorings',   to: 'monitorings#new'
   post 'monitorings', to: 'monitorings#create'
